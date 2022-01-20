21f1de6b5cdf26b7e31db8847ea66d47f284ca43
Paulo Camargo <pcamargobx@gmail.com>
2022-01-19 22:46:00 -0300

    primeiro commit

---
+++ etapas.js
@@ -0,0 +1,63 @@
+let etapas = [
+                {
+                    titulo: 'VEREADOR',
+                    numeros: 5,
+                    candidatos: [
+                        {
+                                numero:  '12345',
+                                nome: 'Nicolas Fury (Nick Fury)',
+                                partido:   'Shield',
+                                fotos:  [
+                                        {url:'12345.jpg', legenda: 'Vereador', small: false}
+                                        ]    
+                                    },
+                        {
+                                
+                                numero:  '67890',
+                                nome: 'Gamora (Gamora)',
+                                partido: 'Guardiões da Galaxia',
+                                fotos:  [
+                                        {url:'67890.jpg', legenda: 'Vereador', small: false}
+                                        ]
+                        },
+                    ]
+                },
+                {
+                    titulo: 'PREFEITO',
+                    numeros: 2,
+                    candidatos : [
+                        {
+                            numero: '01',
+                            nome: 'Steve Rogers (Capitão América)',
+                            partido: 'Vingadores',
+                            vice: 'Bucky',
+                            fotos:  [
+                                    { url: '01.jpg', legenda: 'Prefeito' },
+                                    { url: '01_1.jpg', legenda: 'Vice-Prefeito', small: true }
+                                    ]
+                        },
+                        {
+                            numero: '02',
+                            nome: 'Wanda Maximoff (Feiticeira Scarlate)',
+                            partido: 'Hidra',
+                            vice: 'Mercúrio',
+                            fotos:  [
+                                    { url: '02.jpg', legenda: 'Prefeito' },
+                                    { url: '02_1.jpg', legenda: 'Vice-Prefeito', small: true }
+                                    ]
+                        },
+                        {
+                            numero: '03',
+                            nome: 'Peter Parker (Homem Aranha)',
+                            partido: 'Novos Vingadores',
+                            vice: 'Ned Leeds',
+                            fotos:  [
+                                    { url: '03.jpg', legenda: 'Prefeito' },
+                                    { url: '03_1.jpg', legenda: 'Vice-Prefeito', small: true }
+                                    ]
+                        },
+
+
+                    ]
+                }        
+];
